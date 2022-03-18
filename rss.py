7116ce70fb0ac7073dcc804130e6c00bbc077d00
Rookie001MC <nguyenquocthang2004@gmail.com>
2022-03-18 08:36:51 +0700

    More elegant method for fetching RSS feeds

--- rss.py
+++ rss.py
@@ -2,14 +2,23 @@ from re import search
 import feedparser
 import re
 
-moet_rss_url = "https://moet.gov.vn/rss/Pages/index.aspx?ItemID=54"
-answer = ""
-feed = feedparser.parse(moet_rss_url)
+# ItemID 54 == Tin tổng hợp [0]
+# ItemID 56 == Tin Hoạt động của Bộ [1]
+
+moet_rss_urls = [
+    "https://moet.gov.vn/rss/Pages/index.aspx?ItemID=54",
+    "https://moet.gov.vn/rss/Pages/index.aspx?ItemID=56",
+]
+posts = []
 
 
 def main():
-    for i in range(10):
-        if re.search("(H|h)ọc sinh giỏi quốc gia", feed.entries[i].title):
+    for url in moet_rss_urls:
+        posts.extend(feedparser.parse(url).entries)
+
+    for post in posts:
+        print(post.title)
+        if re.search("(H|h)ọc sinh giỏi quốc gia", post.title):
             found = True
             break
         found = False
