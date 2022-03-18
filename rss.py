ebdb61e6a7891c2744a84662dbcfea6a020ce3c1
Rookie001MC <nguyenquocthang2004@gmail.com>
2022-03-18 19:50:49 +0700

    Did some more checking and refactoring.

--- rss.py
+++ rss.py
@@ -2,18 +2,21 @@ import json
 from re import search
 import feedparser
 import re
+import datetime
 
 # ItemID 54 == Tin tổng hợp [0]
 # ItemID 56 == Tin Hoạt động của Bộ [1]
-
 moet_rss_urls = [
     "https://moet.gov.vn/rss/Pages/index.aspx?ItemID=54",
     "https://moet.gov.vn/rss/Pages/index.aspx?ItemID=56",
 ]
 posts = []
+updated_time = ""
+found = False
 
 
 def main():
+    print("Fetching data from the Ministry of Education and Training...")
     for url in moet_rss_urls:
         posts.extend(feedparser.parse(url).entries)
 
@@ -27,13 +30,16 @@ def main():
 
 
 def result_file_write(found, url):
+    updated_time = datetime.datetime.now().strftime("%A, %d/%m/%Y at %H:%M")
     if found == True:
         data = {
+            "updated_time": updated_time,
             "result": 1,
             "url": url,
         }
     else:
         data = {
+            "updated_time": updated_time,
             "result": 0,
         }
 
